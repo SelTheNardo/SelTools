4d6c11896d0edf0e84ff57934b594e6698f4b64f
Sel the Nardo <90299376+SelTheNardo@users.noreply.github.com>
2025-09-07 22:59:18 -0700

    move enum to file

---
+++ SelTools/Serialization/Structs/Endianness.cs
@@ -0,0 +1,18 @@
+namespace SelTools.Serialization.Structs;
+
+/// <summary>
+/// Endianness refers to the sequential order in which bytes are arranged into larger
+/// numerical values when stored in memory or when transmitted over digital links.
+/// </summary>
+public enum Endianness
+{
+    /// <summary>
+    /// The least significant byte (LSB) value, is at the lowest address.
+    /// </summary>
+    LittleEndian,
+
+    /// <summary>
+    /// The most significant byte (MSB) value, is at the lowest address.
+    /// </summary>
+    BigEndian
+}
--- SelTools/Serialization/Structs/StructTools.cs
+++ SelTools/Serialization/Structs/StructTools.cs
@@ -5,23 +5,6 @@ using System.Reflection;
 
 namespace SelTools.Serialization.Structs;
 
-/// <summary>
-/// Endianness refers to the sequential order in which bytes are arranged into larger
-/// numerical values when stored in memory or when transmitted over digital links.
-/// </summary>
-public enum Endianness
-{
-    /// <summary>
-    /// The least significant byte (LSB) value, is at the lowest address.
-    /// </summary>
-    LittleEndian,
-
-    /// <summary>
-    /// The most significant byte (MSB) value, is at the lowest address.
-    /// </summary>
-    BigEndian
-}
-
 public static class StructTools
 {
     public static byte[] Serialize(object obj, Endianness endianness=Endianness.LittleEndian)
